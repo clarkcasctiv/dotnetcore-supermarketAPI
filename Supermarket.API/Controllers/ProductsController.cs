a717d7bcfda29e7b90f75db19b5e88a11e7c09c5
Pujan Bajra <pujanisme@gmail.com>
2019-02-27 17:44:51 +0545

    Added product get method

--- Supermarket.API/Controllers/ProductsController.cs
+++ Supermarket.API/Controllers/ProductsController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
 using Supermarket.API.Resources;
 
 namespace Supermarket.API.Controllers
@@ -22,7 +24,7 @@ namespace Supermarket.API.Controllers
         public async Task<IEnumerable<ProductResource>> ListAsync()
         {
             var products = await _productService.ListAsync();
-            var resources = _mapper.Map<IEnumerable<ProductResource>, IEnumerable<ProductResource>>(products);
+            var resources = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductResource>>(products);
 
             return resources;
 
--- Supermarket.API/Resources/ProductResource.cs
+++ Supermarket.API/Resources/ProductResource.cs
@@ -5,7 +5,7 @@ namespace Supermarket.API.Resources
         public int Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
-        public int UnitOfMeasurement { get; set; }
+        public string UnitOfMeasurement { get; set; }
         public CategoryResource Category { get; set; }
     }
 }
